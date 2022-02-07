0cf2dddd5bc3d447f574bb4dd30225c317f82401
selcuk baki <selcukbaki@windowslive.com>
2022-02-07 15:15:38 +0300

    Initial Commit

---
+++ CarDealer/CarDealerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CarDealerApp.swift
+//  CarDealer
+//
+//  Created by Selcuk Baki on 7/2/22.
+//
+
+import SwiftUI
+
+@main
+struct CarDealerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
